06f808f6d18dd80b77c17e2390fd315588093960
Thomas / <52424822+Tom-KB@users.noreply.github.com>
2023-10-31 16:11:19 +0100

    Add files via upload

--- converter.ml
+++ converter.ml
@@ -19,7 +19,7 @@ let push x = (pile := x :: !pile)
 let pop n = for _=1 to n do pile := List.tl !pile done
 
 (* table de hachage sui stocke les fonctions *)
-let tab_fonctions = Hashtbl.create 13 (*clé de la table de hachage le nom de la fonction et la valeur est le couple type nombre d'argument de la fonction*)
+let tab_fonctions = Hashtbl.create 13
 
 (* générateur d'identifiant If/IfElse *)
 let new_id_if =
@@ -132,56 +132,84 @@ let malloc = List.rev_append [Li (V0, 9); Syscall]
 
 (* Réalise les opérations arithmétique sur les pointeurs *)
 let rec apply_ptr_arith (o : binop) e1 e2 instr =
-  let r1 = (compile_expr e1 instr) in
+  let r1 = (compile_expr e1 true instr) in
   (match e2 with
-      (* Addition et soustraction entre un pointeur et un entier *)
-      | I i -> (match o with
-                | Add -> Addi (A0, A0, 4 * i) :: r1
-                | Sub -> Addi (A0, A0, -4 * i) :: r1
-                | _ -> failwith "Pointer arithmetic error"
-              )
       (* Soustraction de deux pointeurs *)
-      | Val (Var _) -> let new_instr = Move(A1, A0) :: r1 in let r2 = compile_expr e2 new_instr in 
-              (match o with
-                | Sub -> Mflo(A0) :: Div(A0, A1) :: Li(A1, 4) :: Sub (A0, A1, A0) :: r2
-                | _ -> failwith "Pointer arithmetic error"
-            )
-      | _ -> failwith "Pointer arithmetic error"
+      | Val (Var x) -> let new_instr = Move(A1, A0) :: r1 in let r2 = compile_expr e2 true new_instr in 
+              (
+                match var_type x !pile with
+                  | P _ -> (
+                            match o with
+                              | Sub -> Mflo(A0) :: Div(A0, A1) :: Li(A1, 4) :: Sub (A0, A1, A0) :: r2
+                              | _ -> failwith "Pointer arithmetic error"
+                          )
+                  | _ -> (
+                          match o with
+                            | Add -> Add (A0, A1, A0) :: Mflo (A0) :: Mult (A0, T 0) :: Li (T 0, 4) :: r2
+                            | Sub -> Sub (A0, A1, A0) :: Mflo (A0) :: Mult (A0, T 0) :: Li (T 0, 4) :: r2
+                            | _ -> failwith "Pointer arithmetic error"
+                         )
+              )
+              
+      (* Addition et soustraction entre un pointeur et un entier *)
+      | _ -> r1 |> ~:(Move(T 0, A0)) |> (compile_expr e2 true) |> 
+                (
+                match o with
+                  | Add -> ~:(Add (A0, T 0, A0))
+                  | Sub -> ~:(Sub (A0, T 0, A0))
+                  | _ -> failwith "Pointer arithmetic error"
+                )
     )
 (* ---------------- *)  
 
 (* expr -> instruction list -> instruction list *)
 (* Met dans A0 le résultat de l'expression. *)
-and compile_expr ex instr = match ex with
- | I i -> Li(A0, i) :: instr
+and compile_expr ex with_ptr instr = match ex with
+ | I i -> if with_ptr then 
+            Li(A0, 4 * i) :: instr
+          else  
+            Li(A0, i) :: instr
  | Val (Var x) -> value x instr
- | Moins e -> instr |> (compile_expr e) |> ~:(Sub(A0, Zero, A0))
+ | Moins e -> instr |> (compile_expr e with_ptr) |> ~:(Sub(A0, Zero, A0))
  | Not e ->
-   instr
-   |> (compile_expr e)
-   |> ~:(Slti(A1, A0, 0)) (* Vérifie si A0 est  strictement positif *)
-   |> ~:(Slt(A0, Zero, A0)) (*Vérifie si A0 est strictement negatif*)
-   |> ~:(Nor(A0, A0, A1)) (* =1 si et seulement si A0 était nul donc fait la négation*)
+    instr
+    |> (compile_expr e with_ptr)
+    |> ~:(Slti(A1, A0, 0))
+    |> ~:(Slt(A0, Zero, A0))
+    |> ~:(Nor(A0, A0, A1))
 
 (* ---------------- *)
  | Op(o, e1, e2) ->
     let basic_op instructions = instructions 
-                  |> (compile_expr e1)
-                  |> (add_to_pile Int "1") (* on ajoute le res de e1 sous forme de variable nommé 1 *)
+                  |> (compile_expr e1 with_ptr)
+                  |> (add_to_pile Int "1") (* on ajoute le res de e1 sous forme de variable nommée 1 *)
                   |> (assign "1")
-                  |> (compile_expr e2)
+                  |> (compile_expr e2 with_ptr)
                   |> ~:(Lw(A1, Areg (0, SP))) (* on met le res de e1 dans A1 *)
                   |> (rem_from_pile 1)
-                  |> (apply o A1 A0) (*renvoie les instructions mips qui font l'opération *)
+                  |> (apply o A1 A0)
     in
     (
-      match e1 with 
-        | Val (Var x) -> (
-                            (* Vérifie si le premier terme est un pointeur ou non *)
-                            match var_type x !pile with 
-                              | P _ -> apply_ptr_arith o e1 e2 instr (* Si oui *)
-                              | _ -> basic_op instr (* Si non *)
-                         )
+      match (e1, e2) with 
+        | (Val (Var x), Val (Var y)) -> (
+                                          (* Vérifie si le premier terme est un pointeur ou non *)
+                                          match (var_type x !pile, var_type y !pile) with 
+                                            | (P _, _) -> apply_ptr_arith o e1 e2 instr (* Si oui *)
+                                            | (_, P _) -> apply_ptr_arith o e1 e2 instr (* Si oui *)
+                                            | _ -> basic_op instr (* Si non *)
+                                        )
+        | (_, Val (Var x)) -> (
+                                (* Vérifie si le premier terme est un pointeur ou non *)
+                                match var_type x !pile with 
+                                  | P _ -> apply_ptr_arith o e2 e1 instr (* Si oui *)
+                                  | _ -> basic_op instr (* Si non *)
+                              )
+        | (Val (Var x), _) -> (
+                                (* Vérifie si le premier terme est un pointeur ou non *)
+                                match var_type x !pile with 
+                                  | P _ -> apply_ptr_arith o e1 e2 instr (* Si oui *)
+                                  | _ -> basic_op instr (* Si non *)
+                              )
         | _ -> basic_op instr
 
     )
@@ -191,15 +219,15 @@ and compile_expr ex instr = match ex with
     instr |> ( match args.(0) with 
                 | Val (Var x) -> (match var_type x !pile with 
                                   | P _ -> failwith "malloc ne prend pas de pointeur"
-                                  | _ -> compile_expr args.(0)
+                                  | _ -> compile_expr args.(0) with_ptr
                               )
-                | _ -> compile_expr args.(0)
+                | _ -> compile_expr args.(0) with_ptr
            ) |> malloc |> ~:(Move(A0, V0)) (* Mets l'adresse de l'espace alloué (qui est dans V0) dans A0 *)
 (* ---------------- *)
 
-  | Ecall(s, args) -> (*Ecall ne concerne que les fonctions avec return*)
-   let typ, nb = Hashtbl.find tab_fonctions s in  (*on récupère le type et les arguments de la fonction *)
-   assert (typ <> Void); 
+  | Ecall(s, args) ->
+   let typ, nb = Hashtbl.find tab_fonctions s in
+   assert (typ <> Void);
    assert (Array.length args = nb);
    instr
    |> (compile_args args)
@@ -207,7 +235,7 @@ and compile_expr ex instr = match ex with
   
   (* ---------------- *)
   (* Déréférencement de pointeur, charge dans A0 la valeur de l'adresse pointée *)
-  | ValPointer e -> Lw(A0, Areg(0, A0)) :: compile_expr e instr
+  | ValPointer e -> Lw(A0, Areg(0, A0)) :: compile_expr e true instr
 
   (* Charge dans A0 l'adresse de la variable ou du pointeur avec La (Locate Adress) *)
   | Address (Var x) -> ( match var_type x !pile with 
@@ -219,8 +247,8 @@ and compile_expr ex instr = match ex with
 and compile_args args instr =
   let res = Array.fold_left (fun instr' e ->
               instr'
-              |> (compile_expr e)
-              |> (add_to_pile Void "0arg") (* ajoute quelque chose à la pile pour retenir qu'un espace à été alloué *)
+              |> (compile_expr e false)
+              |> (add_to_pile Void "0arg")
               |> ~:(Sw(A0, Areg(0, SP)))
              ) instr args
   in
@@ -229,7 +257,7 @@ and compile_args args instr =
 
 
 let print = List.rev_append [Li(V0, 1); Syscall; Li(V0, 11); Li(A0, 10); Syscall]
-let return n = List.rev_append [Lw(RA, Areg(0, SP)); Addi(SP, SP, 4*(1+n)); Jr RA] (* le n stocke le nombre d'argument de la fonction. Ici on les retire de sp pour pouvoir ensuite réécrire par dessus*)
+let return n = List.rev_append [Lw(RA, Areg(0, SP)); Addi(SP, SP, 4*(1+n)); Jr RA]
 
 
 (* Compilation d'un stmt *)
@@ -243,27 +271,22 @@ let rec compile_stmt f d stmt_node instr = match stmt_node with
  (* ---------------- *)
  (* Assignation entre une variable, pointeur ou non, et une expression quelconque *)
   (* Il n'y a pas de vérification concernant l'assignation entre un pointeur et un entier, auquel cas la valeur de l'entier est interprété comme une adresse *)
- | Assign(Val (Var x), e2) -> let instructions = instr |> (compile_expr e2) in assign x instructions
+ | Assign(Val (Var x), e2) -> let instructions = instr |> (compile_expr e2 false) in assign x instructions
 
  (* Assignation entre un pointeur déréférencé et une expression quelconque*)
- | Assign(ValPointer (e1), e2) -> let instructions = instr |> (compile_expr e2) |> ~:(Move(T 0, A0)) in Sw(T 0, Areg(0, A0)) :: (compile_expr e1 instructions)
+ | Assign(ValPointer (e1), e2) -> let instructions = instr |> (compile_expr e2 false) |> ~:(Move(T 0, A0)) in Sw(T 0, Areg(0, A0)) :: (compile_expr e1 true instructions)
 
  (* Erreur lorsqu'une assignation est tenté entre d'autres expressions *)
  | Assign(_, _) -> failwith "Wrong assign"
 
  (* Fonction permettant d'afficher un entier *)
  | Scall("print_int", args) ->
-  assert (Array.length args = 1);
-  instr |> ( match args.(0) with 
-               | Val (Var x) -> (match var_type x !pile with 
-                                   | P _ -> failwith "Print_int ne prend pas de pointeur"
-                                   | _ -> compile_expr args.(0)
-                                )
-               | _ -> compile_expr args.(0)) |> print
+    assert (Array.length args = 1);
+    instr |> compile_expr args.(0) false |> print
  (* ---------------- *)
 
- | Scall(s, args) -> (* Scall ne concerne que les fonctions void*)
-   let typ, nb = Hashtbl.find tab_fonctions s in (* On récupère le type et le nombre d'argument de la fonction  *)
+ | Scall(s, args) ->
+   let typ, nb = Hashtbl.find tab_fonctions s in
    assert (typ = Void);
    assert (Array.length args = nb);
    instr
@@ -274,43 +297,43 @@ let rec compile_stmt f d stmt_node instr = match stmt_node with
    if f.typ = Void
    then failwith "Return in void function"
    else instr
-        |> (compile_expr e)
-        |> ~:(Addi(SP, SP, d*4)) (*On enlève les variables locales *)
+        |> (compile_expr e false)
+        |> ~:(Addi(SP, SP, d*4))
         |> (return (Array.length f.args))
  | If(e, stmt) ->
-   let id_if = new_id_if () in  (* associe un numero distinct a chaque if ou if else *)
-   let suite = "suite" ^ id_if in (* permet de nommer la suite associé à chaque if ou if else*)
+   let id_if = new_id_if () in
+   let suite = "suite" ^ id_if in
    instr
-   |> (compile_expr e)
-   |> ~:(Beq(A0, Zero, suite))  (* Si  A0 est nul saute à suite  *)
+   |> (compile_expr e false)
+   |> ~:(Beq(A0, Zero, suite))
    |> (compile_stmt f d stmt)
    |> ~:(Label suite)
  | IfElse(e, stmt1, stmt2) ->
-   let id_if = new_id_if () in (* associe un numero distinct a chaque if ou if else *)
-   let sinon = "else"  ^ id_if in (* permet de nommer le sinon associé à chaque else*)
-   let suite = "suite" ^ id_if in (* permet de nommer la suite associé à chaque if ou if else*)
+   let id_if = new_id_if () in
+   let sinon = "else"  ^ id_if in
+   let suite = "suite" ^ id_if in
    instr
-   |> (compile_expr e)
-   |> ~:(Beq(A0, Zero, sinon)) (* Si  A0 est nul saute à sinon  *)
+   |> (compile_expr e false)
+   |> ~:(Beq(A0, Zero, sinon))
    |> (compile_stmt f d stmt1)
-   |> ~:(J suite) (* saute aux instructions qui suivent le if else et permet donc d'éviter le else*)
+   |> ~:(J suite)
    |> ~:(Label sinon)
    |> (compile_stmt f d stmt2)
    |> ~:(Label suite)
  | While (e, stmt) ->
-   let id_while = new_id_while () in (* on donne un identifiant distinct à chaque while*)
-   Stack.push id_while pile_while;  (* on ajoute l'identifiant a la pile des identifiants des whiles en cours*)
-   let debwhile = "while" ^ id_while in (* On nomme différemment chaque fonction while*)
-   let endwhile = "done"  ^ id_while in (* on nomme différemment chaque suite de while *)
+   let id_while = new_id_while () in
+   Stack.push id_while pile_while;
+   let debwhile = "while" ^ id_while in
+   let endwhile = "done"  ^ id_while in
    instr
    |> ~:(Label debwhile)
-   |> (compile_expr e)
-   |> ~:(Beq(A0, Zero, endwhile)) (*Si A0 est nul saute a la suite du wile*)
+   |> (compile_expr e false)
+   |> ~:(Beq(A0, Zero, endwhile))
    |> (compile_stmt f d stmt)
-   |> ~:(J debwhile) (* On revient au début de la boucle while*)
-   |> ~:(ignore(Stack.pop pile_while); Label endwhile) (* on enleve de la pile l'identifant du while avant de sortir du while*)
- | Continue -> J ("while" ^ (Stack.top pile_while)) :: instr (*On retourne au debut du  while dans lequle l'instruction est directement imbriquée *)
- | Break    -> J ("done"  ^ (Stack.top pile_while)) :: instr  (*On sort du while dans lequle l'instruction est directement imbriquée *)
+   |> ~:(J debwhile)
+   |> ~:(ignore(Stack.pop pile_while); Label endwhile)
+ | Continue -> J ("while" ^ (Stack.top pile_while)) :: instr
+ | Break    -> J ("done"  ^ (Stack.top pile_while)) :: instr
 
 (* On compte les defs et on retire le même
    nombre de variable de la pile que de defs *)
@@ -320,7 +343,7 @@ and compile_block f d lst instr = match lst with
  | (h, _) :: t ->          instr |> (compile_stmt f d h)     |> (compile_block f d t)
 
 
-let verify_arg_name args = (* On vérifie que les arguments ne sont pas des voids et deux fois le meme nom d'argument *)
+let verify_arg_name args =
   for i = 0 to (Array.length args) - 1 do
    if fst args.(i) = Void
    then failwith "void type in argument";
@@ -331,9 +354,9 @@ let verify_arg_name args = (* On vérifie que les arguments ne sont pas des void
   done
 
 (* func -> instruction list -> instruction list *)
-let compile_obj obj instr = match obj with (* on compile les variables globales et les fonctions*)
+let compile_obj obj instr = match obj with
  (* ---------------- *)
- | V(typ, name) -> ( (* on compile les variables globales *) 
+ | V(typ, name) -> (
                     match typ with 
                       (* Ajoute au champ data un pointeur *)
                       | P _ -> push_data name; add_to_pile typ name instr
@@ -341,25 +364,25 @@ let compile_obj obj instr = match obj with (* on compile les variables globales 
                       | _ -> add_to_pile typ name instr
                    )
  (* ---------------- *)
- | F f ->  (* on compile les fonctions*)
-   verify_arg_name f.args; () (* On vérifie que les arguments ne sont pas des voids et deux fois le meme nom d'argument *)
+ | F f ->
+   verify_arg_name f.args;
    let nb_arg =  Array.length f.args in
-   Hashtbl.add tab_fonctions f.name (f.typ, nb_arg); (*clé de la table de hachage le nom de la fonction et la valeur est le couple type nombre d'argument de la fonction*)
-   Array.iter (fun (typ, name) -> push (typ, name, true)) f.args; (* On ajoute les arguments sur la pile *)
+   Hashtbl.add tab_fonctions f.name (f.typ, nb_arg);
+   Array.iter (fun (typ, name) -> push (typ, name, true)) f.args;
    instr
    |> ~:(Label f.name)
-   |> ~:(Move(A0, RA))  
-   |> (add_to_pile Int "0RA") (*Sert a retenir la position de RA  *)
+   |> ~:(Move(A0, RA))
+   |> (add_to_pile Int "0RA")
    |> (assign "0RA")
    |> (compile_stmt f 0 f.body)
    |> ~:(Lw(RA, Areg(0, SP)))
-   |> (rem_from_pile (1 + nb_arg)) (* On enlève de la pile RA et les arguments*)
+   |> (rem_from_pile (1 + nb_arg))
    |> ~:(Jr RA)
 
 
 let rec compile_prog prog instr = match prog with
  | [] -> failwith "programme vide"
- | [F f] when f.name = "main" -> J "main" :: List.rev_append (compile_obj (F f) instr) [Li(V0, 10); Syscall] (*On veut que la dernière fonction soit un main. On saute au main et on renverse notre liste d'instruction *)
+ | [F f] when f.name = "main" -> J "main" :: List.rev_append (compile_obj (F f) instr) [Li(V0, 10); Syscall]
  | [_] -> failwith "Dernier objet non main"
  | obj :: t -> compile_prog t (compile_obj obj instr)
 
